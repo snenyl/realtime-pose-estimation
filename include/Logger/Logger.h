fc165cf3718196f10e8b6d222e74edfbb9b92b5c
simon.nylund <simon.nylund@redrock.no>
2022-04-07 11:16:26 +0200

    Added: Boost logger
    Former-commit-id: 9c9b7d1c844868ab46881831399e40f437c34ccd

--- include/Logger/Logger.h
+++ include/Logger/Logger.h
@@ -2,6 +2,8 @@
 // Created by nylund on 07.04.2022.
 //
 
+#include <boost/log/trivial.hpp>
+
 #ifndef REALTIME_POSE_ESTIMATION_INCLUDE_LOGGER_LOGGER_H_
 #define REALTIME_POSE_ESTIMATION_INCLUDE_LOGGER_LOGGER_H_
 
